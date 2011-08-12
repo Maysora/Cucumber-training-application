9b2de5ef0abb9f5632bac516fc9bd6cb0570ce3f
Roy <roy@kiranatama.com>
2011-08-12 19:55:57 +0700

    delete existing rails 2 app

--- RailsApplication1/config/initializers/cookie_verification_secret.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
-# no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.cookie_verifier_secret = '8c6318abac77182007f1c06146dd1886aac134c707c39c9dff3761c2e9a7add3ca3e57bb7b7e397609d29df1bd247ef96b9517d26785fdaf946dfb6a009d002c';
