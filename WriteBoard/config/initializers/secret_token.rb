ca9dd20bad1252217993e9087143a9cf5028b9f6
Roy <roy@kiranatama.com>
2011-08-18 18:37:34 +0700

    write board initial commit

---
+++ WriteBoard/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WriteBoard::Application.config.secret_token = '6f2d8468701228a3e2c77597626cef7d9b0e4cb348c80f9589035edd7e598dd36f0584663806f46b8fdb4fc5f8cffb951e1f42c94bda370d8c1b4b6e7c3cec32'
